0a159124d92749d9191a599175a46ec2bd583d81
tykwon97 <tykwon97@naver.com>
2023-04-24 10:17:56 +0900

    Feat: DTO에 Final 추가

--- backend/src/main/java/com/rollwrite/domain/meeting/dto/AddMeetingRequestDto.java
+++ backend/src/main/java/com/rollwrite/domain/meeting/dto/AddMeetingRequestDto.java
@@ -3,15 +3,17 @@ package com.rollwrite.domain.meeting.dto;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @Getter
 @ToString
+@RequiredArgsConstructor
 public class AddMeetingRequestDto {
 
-    private String title;
-    private List<Long> tag;
-    private LocalDate startDay;
-    private LocalDate endDay;
-    private String color;
+    private final String title;
+    private final List<Long> tag;
+    private final LocalDate startDay;
+    private final LocalDate endDay;
+    private final String color;
 }
--- backend/src/main/java/com/rollwrite/domain/meeting/dto/AddMeetingResponseDto.java
+++ backend/src/main/java/com/rollwrite/domain/meeting/dto/AddMeetingResponseDto.java
@@ -6,20 +6,22 @@ import java.time.LocalDate;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class AddMeetingResponseDto {
-    private String title;
-    private List<Tag> tag;
-    private LocalDate startDay;
-    private LocalDate endDay;
-    private String color;
-    private String inviteUrl;
+    private final String title;
+    private final List<Tag> tag;
+    private final LocalDate startDay;
+    private final LocalDate endDay;
+    private final String color;
+    private final String inviteUrl;
 
     @Builder
-    public AddMeetingResponseDto(Meeting meeting, String inviteUrl) {
+    public AddMeetingResponseDto(Meeting meeting, String inviteUrl, List<Tag> tag) {
         this.title = meeting.getTitle();
-//        this.tag = meeting.getTagList();
+        this.tag = tag;
         this.startDay = meeting.getStartDay();
         this.endDay = meeting.getEndDay();
         this.color = meeting.getColor();
