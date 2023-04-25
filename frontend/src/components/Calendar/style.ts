c83a94006b33bfe0ddae4bd6e2d8adb3a5dbe882
eosun77 <eosun77@gmail.com>
2023-04-25 12:28:50 +0900

    Feat: 홈페이지구성 (#S08P31A508-170)

--- frontend/src/components/Calendar/style.ts
+++ frontend/src/components/Calendar/style.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 const MonthContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   padding: 0px;
+  min-height: 222px;
 `;
 const Header = styled.div`
   display: flex;
@@ -36,9 +36,17 @@ const DayContainer = styled.div<{ isSwipeTop: boolean }>`
   padding: 0px;
   width: 46px;
   height: ${(props) => (props.isSwipeTop ? "32px" : "52px")};
+
+  @media (height < 700px) {
+    height: ${(props) => (props.isSwipeTop ? "32px" : "50px")};
+  }
 `;
 
-const NumberContainer = styled.div<{ isToday: boolean }>`
+const NumberContainer = styled.div<{
+  isToday: boolean;
+  isPicked: boolean;
+  themaColor: string;
+}>`
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -46,9 +54,16 @@ const NumberContainer = styled.div<{ isToday: boolean }>`
   padding: 0px;
   width: 30px;
   height: 30px;
-  background-color: ${(props) => (props.isToday ? "var(--main-color)" : "")};
+  background-color: ${(props) =>
+    props.isPicked
+      ? "var(--main-color)"
+      : props.isToday
+      ? props.themaColor
+      : ""};
   border-radius: 16px;
   font-size: 16px;
+
+  cursor: pointer;
 `;
 
 const SproutContainer = styled.div`
@@ -64,20 +79,44 @@ const SproutContainer = styled.div`
 const PickedDay = styled.div`
   display: flex;
   text-align: start;
-  padding: 24px 30px 0px;
+  padding: 16px 30px 0px;
   width: 100%;
 
   font-size: 16px;
   color: var(--darkgray-color);
+
+  @media (height < 700px) {
+    font-size: 14px;
+    padding: 12px 30px 0px;
+  }
 `;
 
 const PickedQuestion = styled.div`
   display: flex;
   text-align: start;
-  padding: 16px 30px 3vh 30px;
+  padding: 16px 30px;
   width: 100%;
   font-size: 18px;
   font-weight: bold;
+
+  @media (height < 700px) {
+    font-size: 16px;
+    padding: 10 px 30px;
+  }
+`;
+
+const QuestionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (height < 700px) {
+    > button {
+      font-size: 16px;
+      width: 112px;
+      height: 40px;
+    }
+  }
 `;
 
 export {
@@ -89,4 +128,5 @@ export {
   Header,
   PickedDay,
   PickedQuestion,
+  QuestionContainer,
 };
