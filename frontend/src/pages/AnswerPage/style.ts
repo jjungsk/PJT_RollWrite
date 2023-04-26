ddc30374e1fd850e8fcf149f2ce6d99622fcaf9e
k3020044 <k3020044@gmail.com>
2023-04-26 17:28:58 +0900

    Design: 답변 작성 페이지 css 일부 수정

--- frontend/src/pages/AnswerPage/style.ts
+++ frontend/src/pages/AnswerPage/style.ts
@@ -10,16 +10,36 @@ const NameContainer = styled.div`
 
 const QuestionDiv = styled.div`
   width: 100%;
-  margin-block: 5px;
+  margin: 15px 0;
   padding: 0 15px;
   font-size: 20px;
   font-weight: 700;
   text-align: center;
-
+  line-height: 25px;
 `;
 
+const ImgContainer = styled.div`
+  display: flex;
+  width: 300px;
+  height: 15vh;
+  background: rgba(0, 0, 0, 0.4);
+  opacity: 0.8;
+  border: 1px solid #7A7E80;
+  border-radius: 10px;
+  margin: auto;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const BtnContainer = styled.div`
+  position: absolute;
+  top: 10%;
+`;
 
 export {
     NameContainer, 
     QuestionDiv,
+    ImgContainer,
+    BtnContainer
 };
