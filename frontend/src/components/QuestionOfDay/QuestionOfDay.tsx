93b25179fabfb796dda2e7cc8b52fa223a87e949
eosun77 <eosun77@gmail.com>
2023-04-27 16:04:38 +0900

    Feat: 질문보기 예외처리

--- frontend/src/components/QuestionOfDay/QuestionOfDay.tsx
+++ frontend/src/components/QuestionOfDay/QuestionOfDay.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { format } from "date-fns";
+import { format, isAfter, isBefore, isSameDay, isSameMonth } from "date-fns";
 import { ko } from "date-fns/locale";
 
 import {
@@ -9,20 +9,32 @@ import {
   QuestionOfDayFooter,
 } from "./style";
 import GhostBtn from "../../elements/Button/GhostBtn";
+import { GroupInfo } from "../../constants/types";
 
 interface Props {
   pickedDay: Date;
   questionMap: Map<string, string>;
+  groupInfo: GroupInfo;
 }
 
-function QuestionOfDay({ pickedDay, questionMap }: Props) {
+function QuestionOfDay({ pickedDay, groupInfo, questionMap }: Props) {
   return (
     <QuestionOfDayContainer>
       <QuestionOfDayHeader>
         {format(pickedDay, "PPP", { locale: ko })}
       </QuestionOfDayHeader>
       <QuestionOfDayContent>
-        {questionMap.get(format(pickedDay, "yyyy-MM-dd"))}
+        {(isAfter(pickedDay, new Date(groupInfo.startDay)) &&
+          isBefore(pickedDay, new Date(groupInfo.endDay))) ||
+        isSameDay(pickedDay, new Date(groupInfo?.startDay))
+          ? isAfter(pickedDay, new Date())
+            ? "질문을 기다려라"
+            : questionMap.has(format(pickedDay, "yyyy-MM-dd"))
+            ? questionMap.get(format(pickedDay, "yyyy-MM-dd"))
+            : isSameDay(pickedDay, new Date())
+            ? "오늘 질문은 답변했냐?"
+            : "답변안했네"
+          : "모임기간이 아닙니다."}
       </QuestionOfDayContent>
       <QuestionOfDayFooter>
         <GhostBtn label={"질문 만들기"} />
