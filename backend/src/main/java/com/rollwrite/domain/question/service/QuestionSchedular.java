73c7b82062eb2ed2c330fb49b25297657f6319ea
jangyejoo <jyj01068662778@gmail.com>
2023-04-27 22:52:48 +0900

    Feat: 질문 생성 Schedular 적용 (오전 8시)

---
+++ backend/src/main/java/com/rollwrite/domain/question/service/QuestionSchedular.java
@@ -0,0 +1,33 @@
+package com.rollwrite.domain.question.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class QuestionSchedular {
+
+    private final JobLauncher jobLauncher;
+    private final Job questionJob;
+
+    // 매일 오전 8시
+    @Scheduled(cron = "0 0 8 * * *")
+    public void runJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        log.info("Starting the batch job");
+        Map<String, JobParameter> parameters = new HashMap<>();
+        parameters.put("timestamp", new JobParameter(System.currentTimeMillis()));
+        JobExecution jobExecution = jobLauncher.run(questionJob, new JobParameters(parameters));
+        log.info("Batch job " + jobExecution.getStatus());
+    }
+}
