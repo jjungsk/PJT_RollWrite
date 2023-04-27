2512b2060db51a58a5fefc3326d6f49a1314a72d
tykwon97 <tykwon97@naver.com>
2023-04-27 17:32:51 +0900

    Style: 코드 포맷 수정

--- backend/src/main/java/com/rollwrite/domain/meeting/controller/MeetingController.java
+++ backend/src/main/java/com/rollwrite/domain/meeting/controller/MeetingController.java
@@ -4,8 +4,10 @@ import com.rollwrite.domain.meeting.dto.*;
 import com.rollwrite.domain.meeting.service.MeetingService;
 import com.rollwrite.global.model.ApiResponse;
 import com.rollwrite.global.model.SuccessCode;
+
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
@@ -31,23 +33,23 @@ public class MeetingController {
         log.info("진행 중인 모임 전체 조회");
         Long userId = 1L;
         List<MeetingInProgressResDto> meetingInProgressResDtoList = meetingService.findMeetingInProgress(
-            userId);
+                userId);
         return new ResponseEntity<>(
-            ApiResponse.success(SuccessCode.GET_MEETING_IN_PROGRESS_SUCCESS,
-                meetingInProgressResDtoList),
-            HttpStatus.OK);
+                ApiResponse.success(SuccessCode.GET_MEETING_IN_PROGRESS_SUCCESS,
+                        meetingInProgressResDtoList),
+                HttpStatus.OK);
     }
 
     @PostMapping()
     public ResponseEntity<ApiResponse> addMeeting(
-        @RequestBody AddMeetingRequestDto addMeetingRequestDto) throws NoSuchAlgorithmException {
+            @RequestBody AddMeetingRequestDto addMeetingRequestDto) throws NoSuchAlgorithmException {
         Long userId = 1L;
         log.info("Meeting 생성" + addMeetingRequestDto.toString());
         AddMeetingResponseDto addMeetingResponseDto = meetingService.addMeeting(userId,
-            addMeetingRequestDto);
+                addMeetingRequestDto);
         return new ResponseEntity<>(
-            ApiResponse.success(SuccessCode.ADD_MEETING_SUCCESS, addMeetingResponseDto),
-            HttpStatus.OK);
+                ApiResponse.success(SuccessCode.ADD_MEETING_SUCCESS, addMeetingResponseDto),
+                HttpStatus.OK);
     }
 
     @GetMapping("/{meetingId}")
@@ -56,12 +58,12 @@ public class MeetingController {
         log.info("진행 중인 모임 캘린더 조회  meetingId : " + meetingId + " userId : " + userId);
 
         List<MeetingCalenderResDto> meetingCalenderResDtoList = meetingService.findMeetingCalender(
-            userId, meetingId);
+                userId, meetingId);
 
         return new ResponseEntity<>(
-            ApiResponse.success(SuccessCode.GET_MEETING_CALENDER_SUCCESS,
-                meetingCalenderResDtoList),
-            HttpStatus.OK);
+                ApiResponse.success(SuccessCode.GET_MEETING_CALENDER_SUCCESS,
+                        meetingCalenderResDtoList),
+                HttpStatus.OK);
     }
 
     @GetMapping("/join/{meetingId}")
@@ -80,8 +82,8 @@ public class MeetingController {
         log.info("Meeting 참여자 추가 meetingId : " + meetingId + " userId : " + userId);
         meetingService.joinMeeting(userId, meetingId);
         return new ResponseEntity<>(
-            ApiResponse.success(SuccessCode.JOIN_MEETING_SUCCESS),
-            HttpStatus.OK);
+                ApiResponse.success(SuccessCode.JOIN_MEETING_SUCCESS),
+                HttpStatus.OK);
     }
 
     @GetMapping("/tag")
@@ -89,8 +91,8 @@ public class MeetingController {
         log.info("Tag 리스트 가져오기 ");
         List<TagDto> tagDtoList = meetingService.findTag();
         return new ResponseEntity<>(
-            ApiResponse.success(SuccessCode.GET_TAG_SUCCESS, tagDtoList),
-            HttpStatus.OK);
+                ApiResponse.success(SuccessCode.GET_TAG_SUCCESS, tagDtoList),
+                HttpStatus.OK);
     }
 
     @GetMapping("/result")
@@ -98,9 +100,9 @@ public class MeetingController {
         Long userId = 1L;
         log.info("내 모임 결과 전체 조회 userId : " + userId + " " + pageable.toString());
         List<MeetingResultDto> meetingResultDtoList = meetingService.findMeetingResult(userId,
-            pageable);
+                pageable);
         return new ResponseEntity<>(
-            ApiResponse.success(SuccessCode.GET_MEETING_RESULT_SUCCESS, meetingResultDtoList),
-            HttpStatus.OK);
+                ApiResponse.success(SuccessCode.GET_MEETING_RESULT_SUCCESS, meetingResultDtoList),
+                HttpStatus.OK);
     }
 }
