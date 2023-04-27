22a616fa6def915abe3cfe73ff51aca4a74d490f
k3020044 <k3020044@gmail.com>
2023-04-27 17:41:56 +0900

    Feat : 사진 업로드 기능 일부 구현

--- frontend/src/pages/AnswerPage/AnswerPage.tsx
+++ frontend/src/pages/AnswerPage/AnswerPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 import {
   NameContainer,
@@ -26,6 +26,35 @@ export default function AnswerPage() {
   const title = state.title;
   const day = state.day;
   const question = state.question;
+
+  const imgRef = useRef<HTMLInputElement | null>(null);
+  const onImageChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (!e.target.files) {
+        return;
+      }
+      console.log(e.target.files[0].name);
+    },
+    []
+  );
+
+  //   const [imgFile, setImgFile] = useState<string | null>(null);
+  //   const saveImgFile = () => {
+  //     const file = imgRef.current.files[0];
+  //     const reader = new FileReader();
+  //     reader.readAsDataURL(file);
+  //     reader.onloadend = () => {
+  //       setImgFile(reader.result);
+  //     };
+  //   };
+
+  const handleImgBtn = useCallback(() => {
+    if (!imgRef.current) {
+      return;
+    }
+    imgRef.current.click();
+  }, []);
+
   return (
     <>
       <NameContainer>
@@ -34,8 +63,16 @@ export default function AnswerPage() {
       <QuestionDiv>{question}</QuestionDiv>
       <ImgContainer style={{ backgroundImage: "url()" }}>
         <IconContainer>
-          <ImgBtn></ImgBtn>
+          <ImgBtn onClick={handleImgBtn}></ImgBtn>
         </IconContainer>
+        <input
+          type="file"
+          accept="image/*"
+          id="img"
+          ref={imgRef}
+          onChange={onImageChange}
+          style={{ display: "none" }}
+        />
       </ImgContainer>
       <TextArea></TextArea>
       <GhostBtn label="저장하기"></GhostBtn>
