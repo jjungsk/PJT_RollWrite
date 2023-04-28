d3af43af2110842f1d271754944174e0478c5102
eosun77 <eosun77@gmail.com>
2023-04-28 10:51:40 +0900

    Feat: 성공,실패처리구현

--- frontend/src/pages/JoinPage/JoinPage.tsx
+++ frontend/src/pages/JoinPage/JoinPage.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { joinGroup } from "../../apis/home";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function JoinPage() {
   const { inviteCode } = useParams();
+  const navigate = useNavigate();
   console.log(inviteCode);
   if (inviteCode)
     joinGroup(inviteCode)
       .then((res) => {
         console.log(res);
+        navigate("/");
       })
       .catch((err) => {
         console.log(err);
+        navigate(-1);
       });
   return <div>JoinPage</div>;
 }
