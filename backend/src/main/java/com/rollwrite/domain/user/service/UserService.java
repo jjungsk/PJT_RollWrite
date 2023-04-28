d4a4fcb4ee8487620021eaf9b3cc96db7f76fec6
정세권 <jsk33a@gmail.com>
2023-04-28 10:29:36 +0900

    Feat: 공통 User Service
    - 유저 조희 서비스 구현

--- backend/src/main/java/com/rollwrite/domain/user/service/UserService.java
+++ backend/src/main/java/com/rollwrite/domain/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.rollwrite.domain.user.service;
 
+import com.rollwrite.domain.user.dto.AddUserResDto;
 import com.rollwrite.domain.user.entity.User;
+import com.rollwrite.domain.user.entity.UserType;
 import com.rollwrite.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,15 +11,28 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+/**
+ * User Service Logic
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
 
-    public Optional<User> findUserByIdentifier(String identifier) {
-        return userRepository.findByIdentifier(identifier);
+    // User 찾기
+    @Transactional(readOnly = true)
+    public AddUserResDto findUserByIdentifier(String identifier) {
+        User user = userRepository.findByIdentifier(identifier)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다"));
+
+        AddUserResDto addUserResDto = AddUserResDto.builder()
+                .nickname(user.getNickname())
+                .profileImage(user.getProfileImage())
+                .build();
+
+        return addUserResDto;
     }
+
 }
