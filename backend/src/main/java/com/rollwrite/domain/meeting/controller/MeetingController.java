ff81f5b1225af89f07f7de9d5c1927c9f0c8ff28
tykwon97 <tykwon97@naver.com>
2023-04-28 09:28:36 +0900

    Style: 포맷 수정

--- backend/src/main/java/com/rollwrite/domain/meeting/controller/MeetingController.java
+++ backend/src/main/java/com/rollwrite/domain/meeting/controller/MeetingController.java
@@ -78,7 +78,7 @@ public class MeetingController {
 
     @PostMapping("/join/{inviteCode}/{userId}")
     public ResponseEntity<ApiResponse> joinMeeting(@PathVariable String inviteCode, Long userId) {
-       // TODO: Test하기 위해 직접  userId 입력 받음 (후에 userId는 삭제 예정)
+        // TODO: Test하기 위해 직접  userId 입력 받음 (후에 userId는 삭제 예정)
         log.info("Meeting 참여자 추가 userId : " + userId + " inviteCode : " + inviteCode);
         meetingService.joinMeeting(userId, inviteCode);
         return new ResponseEntity<>(
