b946623a64668f39bfded4398ddf6f4642a50555
tykwon97 <tykwon97@naver.com>
2023-04-28 09:28:04 +0900

    Feat: 모임 참여 API 구현

--- backend/src/main/java/com/rollwrite/domain/meeting/repository/MeetingRepository.java
+++ backend/src/main/java/com/rollwrite/domain/meeting/repository/MeetingRepository.java
@@ -4,9 +4,9 @@ import com.rollwrite.domain.meeting.entity.Meeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
-
-
-
+    Optional<Meeting> findByInviteCode(String inviteCode);
 }
