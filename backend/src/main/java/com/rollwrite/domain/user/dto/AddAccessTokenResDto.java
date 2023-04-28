256818ec86192f08a5343d600eab6f42b57a8445
정세권 <jsk33a@gmail.com>
2023-04-28 10:01:24 +0900

    Feat: client로 보내는 accessToken 객체

---
+++ backend/src/main/java/com/rollwrite/domain/user/dto/AddAccessTokenResDto.java
@@ -0,0 +1,16 @@
+package com.rollwrite.domain.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class AddAccessTokenResDto {
+    private String accessToken;
+
+    @Builder
+    public AddAccessTokenResDto(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
