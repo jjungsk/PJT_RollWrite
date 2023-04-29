673b389ced8e9c526b79f1e3535adf0e53c63872
jangyejoo <jyj01068662778@gmail.com>
2023-04-29 17:19:49 +0900

    Feat: AnswerCustomRepository 함수 추가
    - 모임의 참가자별 답변 길이 조회
    - 모임의 참가자별 사진 개수 조회
    - 모임의 참가자의 답변 리스트 조회

--- backend/src/main/java/com/rollwrite/domain/question/repository/AnswerCustomRepository.java
+++ backend/src/main/java/com/rollwrite/domain/question/repository/AnswerCustomRepository.java
@@ -1,13 +1,22 @@
 package com.rollwrite.domain.question.repository;
 
-
 import com.rollwrite.domain.meeting.dto.MeetingCalenderResDto;
 import com.rollwrite.domain.meeting.entity.Meeting;
+import com.rollwrite.domain.question.dto.AnswerLengthSumDto;
+import com.rollwrite.domain.question.dto.ImageCountDto;
+import com.rollwrite.domain.question.entity.Answer;
 import com.rollwrite.domain.user.entity.User;
+
 import java.util.List;
 
 public interface AnswerCustomRepository {
 
     List<MeetingCalenderResDto> findMeetingCalender(User user, Meeting meeting);
+
+    List<AnswerLengthSumDto> findAnswerLengthSumByMeeting(Meeting meeting);
+
+    List<ImageCountDto> findImageCountByMeeting(Meeting meeting);
+
+    List<Answer> findAnswerByUserAndMeeting(User user, Meeting meeting);
 
 }
--- backend/src/main/java/com/rollwrite/domain/question/repository/AnswerCustomRepositoryImpl.java
+++ backend/src/main/java/com/rollwrite/domain/question/repository/AnswerCustomRepositoryImpl.java
@@ -4,10 +4,15 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.rollwrite.domain.meeting.dto.MeetingCalenderResDto;
 import com.rollwrite.domain.meeting.entity.Meeting;
+import com.rollwrite.domain.question.dto.AnswerLengthSumDto;
+import com.rollwrite.domain.question.dto.ImageCountDto;
+import com.rollwrite.domain.question.entity.Answer;
 import com.rollwrite.domain.question.entity.QAnswer;
 import com.rollwrite.domain.question.entity.QQuestion;
 import com.rollwrite.domain.user.entity.User;
+
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,19 +22,60 @@ public class AnswerCustomRepositoryImpl implements AnswerCustomRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    QQuestion question = QQuestion.question;
     QAnswer answer = QAnswer.answer;
+    QQuestion question = QQuestion.question;
 
     @Override
     public List<MeetingCalenderResDto> findMeetingCalender(User user, Meeting meeting) {
         return jpaQueryFactory
-            .select(Projections.constructor(MeetingCalenderResDto.class,
-                answer.question.createdAt.as("day"),
-                answer.question.content.as("question")))
-            .from(answer)
-            .join(answer.question, question)// TODO: .fetchJoin() 안 되는 이유 해결하기
-            .where(answer.user.eq(user))
-            .where(answer.meeting.eq(meeting))
-            .fetch();
+                .select(Projections.constructor(MeetingCalenderResDto.class,
+                        answer.question.createdAt.as("day"),
+                        answer.question.content.as("question")))
+                .from(answer)
+                .join(answer.question, question)// TODO: .fetchJoin() 안 되는 이유 해결하기
+                .where(answer.user.eq(user))
+                .where(answer.meeting.eq(meeting))
+                .fetch();
     }
+
+    @Override
+    public List<AnswerLengthSumDto> findAnswerLengthSumByMeeting(Meeting meeting) {
+        return jpaQueryFactory
+                .select(Projections.constructor(AnswerLengthSumDto.class,
+                        answer.user.as("userId"),
+                        answer.content.length().sum().as("answerLengthSum")
+                ))
+                .from(answer)
+                .where(answer.meeting.eq(meeting))
+                .groupBy(answer.user)
+                .orderBy(answer.content.length().sum().desc())
+                .fetch();
+    }
+
+    @Override
+    public List<ImageCountDto> findImageCountByMeeting(Meeting meeting) {
+        return jpaQueryFactory
+                .select(Projections.constructor(ImageCountDto.class,
+                        answer.user.as("userId"),
+                        answer.count().as("imageCount")
+                ))
+                .from(answer)
+                .where(answer.meeting.eq(meeting))
+                .where(answer.imageUrl.isNotEmpty())
+                .groupBy(answer.user)
+                .orderBy(answer.count().desc())
+                .fetch();
+    }
+
+    @Override
+    public List<Answer> findAnswerByUserAndMeeting(User user, Meeting meeting) {
+        return jpaQueryFactory
+                .select(answer)
+                .from(answer)
+                .where(answer.user.eq(user))
+                .where(answer.meeting.eq(meeting))
+                .orderBy(answer.createdAt.asc())
+                .fetch();
+    }
+
 }
