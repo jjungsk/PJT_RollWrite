f1b85bcf227cd967da1bccebcc4188f3cb070ff5
jangyejoo <jyj01068662778@gmail.com>
2023-04-29 17:22:24 +0900

    Feat: 모임의 마지막 날일 때 질문 생성
    - 고정 질문 생성
    - 통계 생성

--- backend/src/main/java/com/rollwrite/domain/question/service/QuestionBatch.java
+++ backend/src/main/java/com/rollwrite/domain/question/service/QuestionBatch.java
@@ -1,14 +1,24 @@
 package com.rollwrite.domain.question.service;
 
 import com.rollwrite.domain.meeting.entity.Meeting;
+import com.rollwrite.domain.meeting.entity.Participant;
+import com.rollwrite.domain.meeting.entity.Statistics;
+import com.rollwrite.domain.meeting.entity.StatisticsType;
 import com.rollwrite.domain.meeting.repository.MeetingRepository;
 import com.rollwrite.domain.meeting.repository.ParticipantRepository;
+import com.rollwrite.domain.meeting.repository.StatisticsRepository;
+import com.rollwrite.domain.question.dto.AnswerLengthSumDto;
+import com.rollwrite.domain.question.dto.AnswerRecordDto;
+import com.rollwrite.domain.question.dto.ImageCountDto;
+import com.rollwrite.domain.question.entity.Answer;
 import com.rollwrite.domain.question.entity.Question;
 import com.rollwrite.domain.question.entity.QuestionGpt;
 import com.rollwrite.domain.question.entity.QuestionParticipant;
+import com.rollwrite.domain.question.repository.AnswerRepository;
 import com.rollwrite.domain.question.repository.QuestionGptRepository;
 import com.rollwrite.domain.question.repository.QuestionParticipantRepository;
 import com.rollwrite.domain.question.repository.QuestionRepository;
+import com.rollwrite.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
@@ -19,6 +29,7 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -32,8 +43,10 @@ public class QuestionBatch {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+    private final AnswerRepository answerRepository;
     private final MeetingRepository meetingRepository;
     private final QuestionRepository questionRepository;
+    private final StatisticsRepository statisticsRepository;
     private final ParticipantRepository participantRepository;
     private final QuestionGptRepository questionGptRepository;
     private final QuestionParticipantRepository questionParticipantRepository;
@@ -123,6 +136,162 @@ public class QuestionBatch {
                         return RepeatStatus.FINISHED;
                     }
 
+                    // Meeting
+                    Optional<Meeting> meetingOptional = meetingRepository.findById(meetingId);
+
+                    // Meeting이 없으면
+                    if (meetingOptional.isEmpty()) {
+                        executionContext.remove("idx");
+                        executionContext.put("idx", idx + 1);
+                        return RepeatStatus.CONTINUABLE;
+                    }
+                    Meeting meeting = meetingOptional.get();
+
+                    // 마지막 날이면 통계 내기 + 고정 질문
+                    if (meeting.getEndDay().equals(LocalDate.now())) {
+                        // 답변 제일 길게 많이 한 사람
+                        List<AnswerLengthSumDto> answerLengthSumList = answerRepository.findAnswerLengthSumByMeeting(meeting);
+                        List<User> taleteller = new ArrayList<>();
+                        int maxLengthSum = Integer.MIN_VALUE;
+                        for (AnswerLengthSumDto answerLengthSum : answerLengthSumList) {
+                            User user = answerLengthSum.getUser();
+                            int length = answerLengthSum.getAnswerLengthSum();
+
+                            if (length >= maxLengthSum) {
+                                maxLengthSum = length;
+                                taleteller.add(user);
+                            } else {
+                                break;
+                            }
+                        }
+
+                        // 사진 제일 많이 올린 사람
+                        List<ImageCountDto> imageCountDtoList = answerRepository.findImageCountByMeeting(meeting);
+                        List<User> photographer = new ArrayList<>();
+                        Long maxImageCount = Long.MIN_VALUE;
+                        for (ImageCountDto imageCount : imageCountDtoList) {
+                            User user = imageCount.getUser();
+                            Long count = imageCount.getImageCount();
+
+                            if (count >= maxImageCount) {
+                                maxImageCount = count;
+                                photographer.add(user);
+                            } else {
+                                break;
+                            }
+                        }
+
+                        // 연속 답변 최고기록자
+                        // 1. 모임의 참가자 리스트
+                        List<Participant> participantList = participantRepository.findByMeeting(meeting);
+                        List<AnswerRecordDto> answerRecordDtoList = new ArrayList<>();
+
+                        for (Participant participant : participantList) {
+                            // 2. 참가자의 답변 리스트
+                            List<Answer> answerList = answerRepository.findAnswerByUserAndMeeting(participant.getUser(), meeting);
+
+                            // 3. 참가자의 최대 기록
+                            int curRecord = 1;
+                            int participantRecord = 1;
+                            for (int i = 1, size = answerList.size(); i < size; i++) {
+                                LocalDateTime prevTime = answerList.get(i - 1).getCreatedAt();
+                                LocalDateTime curTime = answerList.get(i).getCreatedAt();
+
+                                // 이전 답변과 지금 답변이 24시간 이내로 차이나면 curRecord++, 아니면 1로 초기화
+                                Duration duration = Duration.between(prevTime, curTime);
+                                if (duration.getSeconds() <= 86400) {
+                                    if (++curRecord >= participantRecord) {
+                                        participantRecord = curRecord;
+                                    }
+                                } else {
+                                    curRecord = 1;
+                                }
+                            }
+                            answerRecordDtoList.add(AnswerRecordDto.builder()
+                                    .user(participant.getUser())
+                                    .answerRecord(participantRecord)
+                                    .build());
+                        }
+
+                        // sort
+                        Collections.sort(answerRecordDtoList, new Comparator<>() {
+                            @Override
+                            public int compare(AnswerRecordDto o1, AnswerRecordDto o2) {
+                                // answerRecord 기준 내림차순
+                                int record1 = o1.getAnswerRecord();
+                                int record2 = o2.getAnswerRecord();
+                                if (record1 < record2) {
+                                    return 1;
+                                } else if (record1 > record2) {
+                                    return -1;
+                                }
+                                return 0;
+                            }
+                        });
+
+                        List<User> proGgr = new ArrayList<>();
+                        int maxRecord = Integer.MIN_VALUE;
+                        for (AnswerRecordDto answerRecord : answerRecordDtoList) {
+                            User user = answerRecord.getUser();
+                            int record = answerRecord.getAnswerRecord();
+
+                            if (record >= maxRecord) {
+                                maxRecord = record;
+                                proGgr.add(user);
+                            } else {
+                                break;
+                            }
+                        }
+
+                        // 충분히 긴 시간 9999-01-01 00:00:00.000000
+                        LocalDateTime infiniteTime = LocalDateTime.of(LocalDate.of(9999, 1, 1), LocalTime.MIN);
+
+                        // question insert
+                        Question question = Question.builder()
+                                .content("마지막 고정 질문은 뭐로 할까요?")
+                                .emoji("마지막 이모지")
+                                .meeting(meeting)
+                                .expireTime(infiniteTime)
+                                .build();
+                        questionRepository.save(question);
+
+                        // TODO : 여기 이름 바꿔야 됨!!!
+                        // statistics insert - taleteller
+                        for (User user : taleteller) {
+                            Statistics statistics = Statistics.builder()
+                                    .type(StatisticsType.TALETELLER)
+                                    .meeting(meeting)
+                                    .user(user)
+                                    .build();
+                            statisticsRepository.save(statistics);
+                        }
+
+                        // statistics insert - photographer
+                        for (User user : photographer) {
+                            Statistics statistics = Statistics.builder()
+                                    .type(StatisticsType.PHTOGRAPHER)
+                                    .meeting(meeting)
+                                    .user(user)
+                                    .build();
+                            statisticsRepository.save(statistics);
+                        }
+
+                        // statistics insert - PROGAGLER
+                        for (User user : proGgr) {
+                            Statistics statistics = Statistics.builder()
+                                    .type(StatisticsType.PROGAGLER)
+                                    .meeting(meeting)
+                                    .user(user)
+                                    .build();
+                            statisticsRepository.save(statistics);
+                        }
+
+                        // 다음 모임으로 넘어감
+                        executionContext.remove("idx");
+                        executionContext.put("idx", idx + 1);
+                        return RepeatStatus.CONTINUABLE;
+                    }
+
                     // 해당 모임에서 질문을 한 참여자 중 랜덤 한 명
                     // SELECT qp.user.id FROM QuestionParticipant qp WHERE qp.meeting.id = :meetingId AND qp.isChoosed = false GROUP BY qp.user ORDER BY RAND()
                     Optional<Long> participantId = questionParticipantRepository.findRandomUserByMeetingAndIsChoosed(meetingId, false);
@@ -175,17 +344,6 @@ public class QuestionBatch {
 
                     // 다음날 오전 8시
                     LocalDateTime expireTime = LocalDateTime.of(LocalDate.now().plusDays(1), LocalTime.of(8, 0));
-
-                    // Meeting
-                    Optional<Meeting> meetingOptional = meetingRepository.findById(meetingId);
-
-                    // Meeting이 없으면
-                    if (meetingOptional.isEmpty()) {
-                        executionContext.remove("idx");
-                        executionContext.put("idx", idx + 1);
-                        return RepeatStatus.CONTINUABLE;
-                    }
-                    Meeting meeting = meetingOptional.get();
 
                     // question insert
                     Question question = Question.builder()
