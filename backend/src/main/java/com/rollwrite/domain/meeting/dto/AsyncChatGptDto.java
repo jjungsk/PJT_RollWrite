4826fc6b9fe25d738186a14aaf283a038a0922f5
tykwon97 <tykwon97@naver.com>
2023-05-01 15:41:21 +0900

    Fix: final 추가

--- backend/src/main/java/com/rollwrite/domain/meeting/dto/AsyncChatGptDto.java
+++ backend/src/main/java/com/rollwrite/domain/meeting/dto/AsyncChatGptDto.java
@@ -4,6 +4,11 @@ import lombok.Getter;
 
 @Getter
 public class AsyncChatGptDto {
-    private String question;
-    private String emoji;
+    private final String question;
+    private final String emoji;
+
+    public AsyncChatGptDto(String question, String emoji) {
+        this.question = question;
+        this.emoji = emoji;
+    }
 }
