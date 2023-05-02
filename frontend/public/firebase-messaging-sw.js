3556eb87812cef624185b5950fe4739cdc47be49
Yechan Gong <rhddpcks9814@naver.com>
2023-05-02 13:27:59 +0900

    Feat: FCM 푸쉬 알림 기능 설정

---
+++ frontend/public/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+importScripts('https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.9.1/firebase-messaging.js');
+
+const config =  {
+    apiKey: "AIzaSyBdVfzVYQAjLuiggA0ABVqBx1Kem0Uses4",
+    authDomain: "rollwrite-1c979.firebaseapp.com",
+    projectId: "rollwrite-1c979",
+    storageBucket: "rollwrite-1c979.appspot.com",
+    messagingSenderId: "1078331877928",
+    appId: "1:1078331877928:web:18fd7af7b4b0e90367631d",
+    measurementId: "G-95KFB7ZPTY",
+};
+firebase.initializeApp(config);
+const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = '[RollWrite] '+payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+      icon: '/sample_profile_image.png'
+    };
+    // eslint-disable-next-line no-restricted-globals
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
