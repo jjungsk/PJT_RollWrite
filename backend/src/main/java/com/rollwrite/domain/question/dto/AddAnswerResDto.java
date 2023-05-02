d7a3f172b868ed06daac9d5a9cf43d92eb8bb26c
jangyejoo <jyj01068662778@gmail.com>
2023-05-02 17:04:01 +0900

    Refactor: builder 파라미터 수정

--- backend/src/main/java/com/rollwrite/domain/question/dto/AddAnswerResDto.java
+++ backend/src/main/java/com/rollwrite/domain/question/dto/AddAnswerResDto.java
@@ -1,5 +1,6 @@
 package com.rollwrite.domain.question.dto;
 
+import com.rollwrite.domain.meeting.entity.Participant;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,11 +9,11 @@ import lombok.ToString;
 @ToString
 public class AddAnswerResDto {
     private final Long meetingId;
-    private final boolean isDone;
+    private final Boolean isDone;
 
     @Builder
-    public AddAnswerResDto(Long meetingId, boolean isDone) {
-        this.meetingId = meetingId;
-        this.isDone = isDone;
+    public AddAnswerResDto(Participant participant) {
+        this.meetingId = participant.getMeeting().getId();
+        this.isDone = participant.isDone();
     }
 }
