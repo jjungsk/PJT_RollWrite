8bf3ad27b47f038efdb4090f30c07de12e11594a
eosun77 <eosun77@gmail.com>
2023-05-04 13:47:08 +0900

    Feat: 이모지 변경

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -12,7 +12,7 @@ function ErrorPage() {
   return (
     <ErrorPageContainer>
       <img src="./sad-pepe.gif" alt="sad pepe" />
-      <p>잘못된 접근입니다 :땀_흘리는_웃는_얼굴:</p>
+      <p>잘못된 접근입니다 😥</p>
     </ErrorPageContainer>
   );
 }
