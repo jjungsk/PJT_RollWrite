9e4a7f342b5477abc9a450f989fdfb589da1f32d
jangyejoo <jyj01068662778@gmail.com>
2023-05-04 09:24:43 +0900

    Refactor: ParticipantCustomRepository 변경
    - findTodayMeetingByUser 삭제
    - findMeetingByUserAndMeetingAndIsDone 추가

--- backend/src/main/java/com/rollwrite/domain/meeting/repository/ParticipantCustomRepository.java
+++ backend/src/main/java/com/rollwrite/domain/meeting/repository/ParticipantCustomRepository.java
@@ -5,17 +5,18 @@ import com.rollwrite.domain.meeting.entity.Meeting;
 import java.util.List;
 import java.util.Optional;
 
+import com.rollwrite.domain.meeting.entity.Participant;
 import org.springframework.data.domain.Pageable;
 
 public interface ParticipantCustomRepository {
 
     List<Meeting> findMeetingByUserAndIsDone(Long userId, boolean isDone);
 
-    List<Meeting> findTodayMeetingByUser(Long userId);
-
     List<Meeting> findFinisihedMeetingByUser(Long userId, Pageable pageable);
 
     Optional<Meeting> findMeetingByUserAndMeetingAndIsDone(Long userId, Long meetingId,
                                                            boolean isDone);
+
+    Optional<Participant> findParticipantByUserAndMeetingAndIsDone(Long userId, Long meetingId, boolean isDone);
 
 }
