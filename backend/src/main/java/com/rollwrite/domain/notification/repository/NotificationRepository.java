2f206f4605e716985c0fc46f201c561261075508
jangyejoo <jyj01068662778@gmail.com>
2023-05-04 23:15:28 +0900

    Add: NotificationCustomRepository 추가

--- backend/src/main/java/com/rollwrite/domain/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/rollwrite/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,13 @@
 package com.rollwrite.domain.notification.repository;
 
 import com.rollwrite.domain.notification.entity.Notification;
+import com.rollwrite.domain.notification.entity.NotificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
+import java.util.List;
 
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long>, NotificationCustomRepository {
+    List<Notification> findAllByType(NotificationType type);
 }
