7b3f4dd37cae6013c490ce5a2dbe2dd3711b814e
jangyejoo <jyj01068662778@gmail.com>
2023-05-05 20:01:39 +0900

    Style: log 수정

--- backend/src/main/java/com/rollwrite/domain/admin/controller/AdminController.java
+++ backend/src/main/java/com/rollwrite/domain/admin/controller/AdminController.java
@@ -5,6 +5,7 @@ import com.rollwrite.domain.admin.service.AdminService;
 import com.rollwrite.global.auth.CustomUserDetails;
 import com.rollwrite.global.model.ApiResponse;
 import com.rollwrite.global.model.SuccessCode;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,7 @@ import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
+@Api(tags = {"05. Admin-Controller (관리자 페이지 관련)"})
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -32,7 +34,7 @@ public class AdminController {
 
     @PostMapping("/notice")
     public ResponseEntity<ApiResponse> addNotice(@ApiIgnore Authentication authentication, @RequestBody String content) {
-        log.info("addNotice 호출");
+        log.info("공지 생성 content : {}", content);
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getDetails();
         Long userId = userDetails.getUserId();
         adminService.addNotice(userId, content);
@@ -43,7 +45,7 @@ public class AdminController {
     public ResponseEntity<ApiResponse> modifyNotice(@ApiIgnore Authentication authentication,
                                                     @PathVariable Long noticeId,
                                                     @RequestBody String content) {
-        log.info("modifyNotice 호출");
+        log.info("공지 수정 noticeId : {}, content : {}", noticeId, content);
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getDetails();
         Long userId = userDetails.getUserId();
         adminService.modifyNotice(userId, noticeId, content);
@@ -53,7 +55,7 @@ public class AdminController {
     @DeleteMapping("/notice/{noticeId}")
     public ResponseEntity<ApiResponse> removeNotice(@ApiIgnore Authentication authentication,
                                                     @PathVariable Long noticeId) {
-        log.info("modifyNotice 호출");
+        log.info("공지 삭제 noticeId : {}", noticeId);
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getDetails();
         Long userId = userDetails.getUserId();
         adminService.removeNotice(userId, noticeId);
