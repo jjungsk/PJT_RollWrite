15a99ca9d2fe9a51908daa40982e0db5eb32892d
eosun77 <eosun77@gmail.com>
2023-05-08 13:27:37 +0900

    Feat: 완성이모지 추가

--- frontend/src/pages/CreateGroupPage/CreateGroupPage.tsx
+++ frontend/src/pages/CreateGroupPage/CreateGroupPage.tsx
@@ -116,6 +116,7 @@ function CreateGroupPage() {
         <Info
           title={newGroupInfo?.title}
           subTitle="모임을 만들었어요."
+          emoji="🤝"
           fillLabel="초대하기"
           ghostLabel="홈으로"
           fillOnClick={() => handleKakaoShare(newGroupInfo?.inviteUrl!)}
