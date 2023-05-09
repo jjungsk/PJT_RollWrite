0923456e8ed795ef53ecdc478a648d2e26779f81
Yechan Gong <rhddpcks9814@naver.com>
2023-05-09 09:28:57 +0900

    Feat: Add page title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -107,6 +107,55 @@ function App() {
         dispatch(updateRouteHistory(currentPath));
       }
     }
+
+    const pathParts = currentPath.split("/");
+    const htmlTitle = document.querySelector("title");
+    switch (pathParts[1]) {
+      case "login":
+        htmlTitle!.innerHTML = "로그인 - Rollwrite";
+        break;
+      case "home":
+        htmlTitle!.innerHTML = "홈 - Rollwrite";
+        break;
+      case "question":
+        htmlTitle!.innerHTML = "오늘의 질문 - Rollwrite";
+        break;
+      case "answer":
+        htmlTitle!.innerHTML = "답변 작성하기 - Rollwrite";
+        break;
+      case "my":
+        htmlTitle!.innerHTML = "마이페이지 - Rollwrite";
+        break;
+      case "notify":
+        htmlTitle!.innerHTML = "알림 - Rollwrite";
+        break;
+      case "setting":
+        htmlTitle!.innerHTML = "설정 - Rollwrite";
+        break;
+      case "notice":
+        htmlTitle!.innerHTML = "공지사항 - Rollwrite";
+        break;
+      case "inquiry":
+        htmlTitle!.innerHTML = "문의사항 - Rollwrite";
+        break;
+      case "invite":
+        htmlTitle!.innerHTML = "모임 초대하기 - Rollwrite";
+        break;
+      case "result":
+        htmlTitle!.innerHTML = "모임 상세보기 - Rollwrite";
+        break;
+      case "create":
+        htmlTitle!.innerHTML = "모임 생성하기 - Rollwrite";
+        break;
+      case "join":
+        htmlTitle!.innerHTML = "모임 가입하기 - Rollwrite";
+        break;
+      case "award":
+        htmlTitle!.innerHTML = "모임 결과 - Rollwrite";
+        break;
+      default:
+        break;
+    }
   });
 
   return (
