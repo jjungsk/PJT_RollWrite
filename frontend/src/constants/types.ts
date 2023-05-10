b819286502488d6ef312f42b574c9030202242cf
eosun77 <eosun77@gmail.com>
2023-05-10 10:11:45 +0900

    Fix: 스크린 높이수정

--- frontend/src/constants/types.ts
+++ frontend/src/constants/types.ts
@@ -6,6 +6,8 @@ import { ReactElement } from "react";
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
+export const screenHight = window.innerHeight;
+
 export interface CreateGroup {
   title: string;
   tag: number[];
