113216fccd760c1e4b200406cb920cb892be4800
Yechan Gong <rhddpcks9814@naver.com>
2023-05-15 17:59:38 +0900

    Feat: admin page 기능 추가

--- frontend/src/pages/AdminPage/AdminPageGroup.tsx
+++ frontend/src/pages/AdminPage/AdminPageGroup.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { AdminPageWrapper } from "./style";
-import { Meeting } from "./type";
+import { Meeting, User } from "./type";
 import { createTodayQuestion, getMeetingList } from "./admin";
 import { toast } from "react-hot-toast";
 import {
@@ -12,14 +12,33 @@ import {
   TableBody,
   Paper,
   Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Typography,
+  DialogActions,
+  Divider,
+  Button,
+  useMediaQuery,
 } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 import MapsUgcRoundedIcon from "@mui/icons-material/MapsUgcRounded";
 import AdminPageDialog from "./AdminPageDialog";
 
 function AdminPageGroup() {
   const [meetingList, setMeetingList] = useState<Meeting[]>();
+  const [userList, setUserList] = useState<User[]>();
   const [clickedMeeting, setClickedMeeting] = useState<Meeting>();
   const [open, setOpen] = React.useState(false);
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
 
   useEffect(() => {
     getMeetingList().then((res) => {
@@ -40,64 +59,139 @@ function AdminPageGroup() {
         setMeetingList(res.data);
       });
   };
+
+  const handleClickDialogOpen = (meetingId: number) => {
+    meetingList?.map((meeting: Meeting) => {
+      if (meeting.meetingId === meetingId) {
+        setUserList(meeting.participant);
+        setOpenDialog(true);
+      }
+      return null;
+    });
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+  };
+
   return (
-    <AdminPageWrapper>
-      <AdminPageDialog
-        open={open}
-        setOpen={setOpen}
-        title={clickedMeeting?.title + "에 오늘의 질문을 추가하시겠습니까?"}
-        onAgree={onAgree}
-      />
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>모임 이름</TableCell>
-              <TableCell align="center">시작일</TableCell>
-              <TableCell align="center">종료일</TableCell>
-              <TableCell align="center">테마</TableCell>
-              <TableCell align="center">초대코드</TableCell>
-              <TableCell align="center">태그</TableCell>
-              <TableCell align="center">질문추가</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {meetingList?.map((meeting) => (
-              <TableRow
-                key={meeting.meetingId}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {meeting.title}
-                </TableCell>
-                <TableCell align="center">{meeting.startDay}</TableCell>
-                <TableCell align="center">{meeting.endDay}</TableCell>
-                <TableCell align="center">
-                  <Box
-                    sx={{
-                      width: 16,
-                      height: 16,
-                      backgroundColor: meeting.color,
-                      margin: "auto",
-                    }}
-                  />
-                </TableCell>
-                <TableCell align="center">{meeting.inviteUrl}</TableCell>
-                <TableCell align="center">
-                  {meeting.tag.map(
-                    (t, i) =>
-                      t.content + (i < meeting.tag.length - 1 ? ", " : "")
+    <>
+      {openDialog && (
+        <Dialog
+          fullScreen={fullScreen}
+          open={openDialog}
+          onClose={handleDialogClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">참여자 목록</DialogTitle>
+          <DialogContent>
+            <List
+              sx={{
+                width: "100%",
+                maxWidth: 720,
+                margin: "auto",
+              }}
+            >
+              {userList?.map((user, i) => (
+                <div key={user.userId}>
+                  <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                      <Avatar alt={user.nickname} src={user.profileImage} />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={`${user.nickname} - ${user.point ?? 0}p`}
+                      secondary={
+                        <React.Fragment>
+                          <Typography
+                            sx={{ display: "inline" }}
+                            component="span"
+                            variant="body2"
+                            color="text.primary"
+                          >
+                            User Type -{" "}
+                          </Typography>
+                          {user.userType}
+                        </React.Fragment>
+                      }
+                    />
+                  </ListItem>
+                  {i < userList.length - 1 && (
+                    <Divider variant="inset" component="li" />
                   )}
-                </TableCell>
-                <TableCell align="center">
-                  <MapsUgcRoundedIcon onClick={() => handleClick(meeting)} />
-                </TableCell>
+                </div>
+              ))}
+            </List>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleDialogClose}>닫기</Button>
+          </DialogActions>
+        </Dialog>
+      )}
+      <AdminPageWrapper>
+        <AdminPageDialog
+          open={open}
+          setOpen={setOpen}
+          title={clickedMeeting?.title + "에 오늘의 질문을 추가하시겠습니까?"}
+          onAgree={onAgree}
+        />
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>모임 이름</TableCell>
+                <TableCell align="center">시작일</TableCell>
+                <TableCell align="center">종료일</TableCell>
+                <TableCell align="center">참여자</TableCell>
+                <TableCell align="center">테마</TableCell>
+                <TableCell align="center">초대코드</TableCell>
+                <TableCell align="center">태그</TableCell>
+                <TableCell align="center">질문추가</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </AdminPageWrapper>
+            </TableHead>
+            <TableBody>
+              {meetingList?.map((meeting) => (
+                <TableRow
+                  key={meeting.meetingId}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {meeting.title}
+                  </TableCell>
+                  <TableCell align="center">{meeting.startDay}</TableCell>
+                  <TableCell align="center">{meeting.endDay}</TableCell>
+                  <TableCell
+                    align="center"
+                    onClick={() => handleClickDialogOpen(meeting.meetingId)}
+                  >
+                    {meeting.participantCnt ?? 0}
+                  </TableCell>
+                  <TableCell align="center">
+                    <Box
+                      sx={{
+                        width: 16,
+                        height: 16,
+                        backgroundColor: meeting.color,
+                        margin: "auto",
+                      }}
+                    />
+                  </TableCell>
+                  <TableCell align="center">{meeting.inviteUrl}</TableCell>
+                  <TableCell align="center">
+                    {meeting.tag.map(
+                      (t, i) =>
+                        t.content + (i < meeting.tag.length - 1 ? ", " : "")
+                    )}
+                  </TableCell>
+                  <TableCell align="center">
+                    <MapsUgcRoundedIcon onClick={() => handleClick(meeting)} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </AdminPageWrapper>
+    </>
   );
 }
 
