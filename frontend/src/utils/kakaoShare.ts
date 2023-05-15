0bf31451964f511c3e7cd1111752ac8d532b2c7f
eosun77 <eosun77@gmail.com>
2023-05-15 09:03:02 +0900

    Fix: 카카오톡 공유하기 사진 수정

--- frontend/src/utils/kakaoShare.ts
+++ frontend/src/utils/kakaoShare.ts
@@ -9,8 +9,7 @@ export const handleKakaoInviteShare = (group: Group) => {
       title: `${group.title}에서 당신을 초대합니다.`,
       description:
         "우리 함께한 시간, 소중한 추억을 그냥 흘려보내지 않겠어요! 지금 바로 Rollwrite에 참여하세요! 친구들, 가족, 동료들과의 소중한 시간을 마음을 담아 기록하고,나중에 되돌아봤을 때 따뜻한 미소를 띄울 수 있는 그 날의 기억을 되살릴 수 있습니다.",
-      imageUrl:
-        "https://rollwrite.co.kr/rollwrite/profile/0b473d60-f505-4338-8cfe-aa15e0214be1rainbow.gif",
+      imageUrl: "https://rollwrite.co.kr/logo_square.png",
       link: {
         mobileWebUrl: shareUrl,
         webUrl: shareUrl,
@@ -36,8 +35,7 @@ export const handleKakaoQuestionShare = (question: CalendarQuestion) => {
     content: {
       title: `오늘질문 빨리 답변 부탁드립니다 🙏`,
       description: `오늘의 질문 : ${question.question}`,
-      imageUrl:
-        "https://rollwrite.co.kr/rollwrite/profile/0b473d60-f505-4338-8cfe-aa15e0214be1rainbow.gif",
+      imageUrl: "https://rollwrite.co.kr/logo_square.png",
       link: {
         mobileWebUrl: shareUrl,
         webUrl: shareUrl,
