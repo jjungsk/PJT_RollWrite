8d2ad403bf7150d7351c3f3ba9e656c46292de36
Yechan Gong <rhddpcks9814@naver.com>
2023-05-18 11:09:17 +0900

    Feat: 서비스 소개 페이지 구현

--- frontend/src/pages/ManualPage/ManualPage.tsx
+++ frontend/src/pages/ManualPage/ManualPage.tsx
@@ -1,111 +1,137 @@
 import React, { useState } from "react";
+import { ReactComponent as PresentBox } from "../../assets/Box.svg";
+import { ReactComponent as Home } from "../../assets/Home.svg";
+import { ReactComponent as Logo } from "../../assets/Logo.svg";
+import Box from "../../components/Atom/Box/Box";
 import Tabs from "../../components/Molecules/Tabs/Tabs";
+import {
+  ManualContainer,
+  ManualImg,
+  ManualStep,
+  ManualStepBox,
+  ManualStepText,
+  ManualSubTitle,
+  ManualWrap,
+} from "./style";
+import {
+  BackNavigationContainer,
+  BackNavigationTitle,
+} from "../../components/Organism/BackNavigation/style";
+import { useNavigate } from "react-router-dom";
 
 function ManualPage() {
-  const menuTabList = ["기본", "생성/초대", "질문/답변", "모임", "결과"];
+  const navigate = useNavigate();
+  const menuTabList = ["소개", "생성/초대", "모임", "질문/답변", "결과"];
   const [selectedMenuIndex, setSelectedMenuIndex] = useState<number>(0);
 
   return (
-    <div style={{ backgroundColor: "white", height: "100%" }}>
+    <ManualWrap>
+      <BackNavigationContainer>
+        <Home onClick={() => navigate("/")} />
+        <BackNavigationTitle>서비스 소개</BackNavigationTitle>
+        <Box width="32px" height="32px" />
+      </BackNavigationContainer>
       <Tabs
         menuTabList={menuTabList}
         selectedMenuIndex={selectedMenuIndex}
         setSelectedMenuIndex={setSelectedMenuIndex}
       />
-      {selectedMenuIndex === 0 && (
-        <>
-          <div>
-            Rollwrite는
-            <br />
-            여러분이 모인 모든 순간을
-            <br />
-            영원히 간직할 수 있는 롤링페이퍼 플랫폼입니다
-          </div>
-          <div>
-            <div>규칙 1. </div>
-            <div>
-              매일 아침 8시에 올라오는 질문에 답변해주세요
+      <ManualContainer>
+        {selectedMenuIndex === 0 && (
+          <>
+            <PresentBox />
+            <Logo />
+            <ManualSubTitle>
+              Rollwrite는 여러분이 모인
               <br />
-              (질문은 우리 모임에 맞게 자동 생성 되며 질문 추가도 가능해요)
-            </div>
-          </div>
-          <div>
-            <div>규칙 2. </div>
-            <div>
-              답변을 달지 않으면 우리의 추억이 공유되지 않아요ㅠ-ㅠ
+              모든 순간을 영원히 간직할 수 있는
               <br />
-              (다음날 질문이 올라오기 전까지 답변을 달아주세요)
-            </div>
-          </div>
-          <div>
-            <div>규칙 3. </div>
-            <div>
-              모든 답변은 모임의 마지막 날 확인 가능해요
+              롤링페이퍼 플랫폼입니다
+            </ManualSubTitle>
+            <ManualStepBox>
+              <ManualStep>규칙 1. </ManualStep>
+              <ManualStepText>
+                <div>매일 아침 8시에 올라오는 질문에 답변해주세요</div>
+                <div>
+                  (질문은 우리 모임에 맞게 자동 생성 되며 질문 추가도 가능해요)
+                </div>
+              </ManualStepText>
+            </ManualStepBox>
+            <ManualStepBox>
+              <ManualStep>규칙 2. </ManualStep>
+              <ManualStepText>
+                <div>답변을 달지 않으면 우리의 추억이 공유되지 않아요</div>
+                <div>(다음날 질문이 올라오기 전까지 답변을 달아주세요)</div>
+              </ManualStepText>
+            </ManualStepBox>
+            <ManualStepBox>
+              <ManualStep>규칙 3. </ManualStep>
+              <ManualStepText>
+                <div>모든 답변은 모임의 마지막 날 확인 가능해요</div>
+                <div>(마지막 날 우리 모임의 선물이 될거에요^^)</div>
+              </ManualStepText>
+            </ManualStepBox>
+          </>
+        )}
+        {selectedMenuIndex !== 0 && (
+          <ManualImg
+            src={`/manual_step_${selectedMenuIndex}_bg.png`}
+            alt="manualImage"
+          />
+          // <ManualImg src={`/manual_step_${selectedMenuIndex}.png`} alt="manualImage"/>
+        )}
+        {selectedMenuIndex === 1 && (
+          <ManualStepBox>
+            <ManualStep>Step 1. </ManualStep>
+            <ManualStepText>
+              "프로필" 안에 "+" 버튼을 클릭해서 우리들만의 모임을
               <br />
-              (마지막 날 우리 모임의 선물이 될거에요^^)
-            </div>
-          </div>
-        </>
-      )}
-      {selectedMenuIndex !== 0 && (
-        <img
-          src={`/manual_step_${selectedMenuIndex}.png`}
-          alt="manualImage"
-          style={{ width: "calc(100% - 20px)", margin: "auto" }}
-        />
-      )}
-      {selectedMenuIndex === 1 && (
-        <>
-          <div>
-            <div>Step 1. </div>
-            <div>
-              "프로필" 안에 "+" 버튼을 클릭해서 우리들만의 모임을 만들고 친구를
-              초대 해주세요
-            </div>
-          </div>
-        </>
-      )}
-      {selectedMenuIndex === 2 && (
-        <>
-          <div>
-            <div>Step 2. </div>
-            <div>
-              "홈" 화면 또는 "모임 상세보기"를 클릭해서 답변을 달아주세요
-              (10point가 적립 돼요)
-              <br />+ 모임 상세에서 "질문 하기"를 클릭해서 우리 친구들에게
-              궁금한 점을 질문 해주세요
-            </div>
-          </div>
-        </>
-      )}
-      {selectedMenuIndex === 3 && (
-        <>
-          <div>
-            <div>Step 3. </div>
-            <div>
-              "모임 상세보기"에서 다양한 정보를 확인할 수 있어요
+              만들고 친구를 초대 해주세요
+            </ManualStepText>
+          </ManualStepBox>
+        )}
+        {selectedMenuIndex === 2 && (
+          <ManualStepBox>
+            <ManualStep>Step 2. </ManualStep>
+            <ManualStepText>
+              "모임 상세보기"에서 모임 기간, 모임 멤버, 답변률 등
               <br />
-              * 답변률, 모임 기간, 모임 멤버 등
-              <br />* 답변 뽑기 : 10point를 사용해 무작위으로 답변 확인 가능해요
-            </div>
-          </div>
-        </>
-      )}
-      {selectedMenuIndex === 4 && (
-        <>
-          <div>
-            <div>Step 4. </div>
-            <div>
+              다양한 정보를 확인할 수 있어요
+              <div>
+                * 답변 뽑기 : 10point를 사용해 무작위으로 답변 확인 가능해요
+              </div>
+            </ManualStepText>
+          </ManualStepBox>
+        )}
+        {selectedMenuIndex === 3 && (
+          <ManualStepBox>
+            <ManualStep>Step 3. </ManualStep>
+            <ManualStepText>
+              "홈" 화면 또는 "모임 상세보기"를 클릭해서 답변을
+              <br />
+              달아주세요 (10point가 적립 돼요)
+              <br />+ 모임 상세에서 "질문 하기"를 클릭해서 우리
+              <br />
+              친구들에게 궁금한 점을 질문 해주세요
+            </ManualStepText>
+          </ManualStepBox>
+        )}
+        {selectedMenuIndex === 4 && (
+          <ManualStepBox>
+            <ManualStep>Step 4. </ManualStep>
+            <ManualStepText>
               모임의 마지막 우리의 소중한 추억을 확인해 보세요^^
               <br />
-              (마지막 답변을 달지 않으면 롤링페이퍼가 보이지 않아요)
-              <br />+ "선물 상자"를 클릭하면 모임 결과 이벤트를 다시 볼 수
-              있어요~!
-            </div>
-          </div>
-        </>
-      )}
-    </div>
+              <div>(마지막 답변을 달지 않으면 롤링페이퍼가 보이지 않아요)</div>
+              <div>
+                + "선물 상자"를 클릭하면 모임 결과 이벤트를 다시
+                <br />볼 수 있어요~!
+              </div>
+            </ManualStepText>
+          </ManualStepBox>
+        )}
+      </ManualContainer>
+    </ManualWrap>
   );
 }
 
---
+++ frontend/src/pages/ManualPage/style.ts
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+
+const ManualWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #f0ede6;
+
+  & > div:nth-child(2) {
+    margin-top: 4px;
+    padding: 0 8px;
+  }
+`;
+
+const ManualContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  font-weight: bold;
+
+  & > svg {
+    width: 240px;
+    height: fit-content;
+    margin: 10px auto;
+  }
+  & > svg:nth-child(2) {
+    width: 200px;
+    margin: 20px auto;
+  }
+`;
+
+const ManualSubTitle = styled.div`
+  font-size: 20px;
+  line-height: 28px;
+`;
+
+const ManualImg = styled.img`
+  width: calc(100% - 10px);
+  margin: 40px auto 20px;
+  border-radius: 5px;
+`;
+
+const ManualStepBox = styled.div`
+  display: flex;
+  width: 340px;
+  font-size: 14px;
+  line-height: 22px;
+  text-align: left;
+  margin: 30px auto;
+
+  @media all and (min-width: 380px) {
+    width: 380px;
+    font-size: 15px;
+    line-height: 24px;
+  }
+`;
+
+const ManualStep = styled.div`
+  width: 46px;
+
+  @media all and (min-width: 380px) {
+    width: 52px;
+  }
+`;
+
+const ManualStepText = styled.div`
+  & > div:nth-child(2) {
+    font-size: 11px;
+    line-height: 12px;
+    margin-top: 6px;
+  }
+  & > div:nth-child(3) {
+    margin-top: 6px;
+  }
+
+  @media all and (min-width: 380px) {
+    & > div:nth-child(2) {
+      font-size: 13px;
+      line-height: 16px;
+      margin-top: 8px;
+    }
+    & > div:nth-child(3) {
+      margin-top: 8px;
+    }
+  }
+`;
+
+export {
+  ManualWrap,
+  ManualContainer,
+  ManualSubTitle,
+  ManualImg,
+  ManualStepBox,
+  ManualStep,
+  ManualStepText,
+};
